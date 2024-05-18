998c2afb4d9a47d93848635e85f370b70314fdc0
Nathan Mande <neodarksoulink@gmail.com>
2024-05-18 19:35:32 +0200

    feat: worker threads in action

---
+++ src/fibonacci/fibonacci.controller.ts
@@ -0,0 +1,16 @@
+import Piscina from 'piscina';
+
+import { Controller, Get, Query } from '@nestjs/common';
+import { resolve } from 'path';
+
+@Controller('fibonacci')
+export class FibonacciController {
+  fibonacciWorker = new Piscina({
+    filename: resolve(__dirname, 'fibonacci.worker.js'),
+  });
+
+  @Get()
+  fibonacci(@Query('n') n = 10) {
+    return this.fibonacciWorker.run(n);
+  }
+}
